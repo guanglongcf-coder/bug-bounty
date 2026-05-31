c40c556ffa882f9c8b6bfe3d03b10d95087cb381
guanglongcf-coder <guanglongcf-coder@users.noreply.github.com>
2026-05-31 09:51:50 +0800

    fix: add auth middleware to reviewRoutes.js (#2425)
    Parent bounty: #743

--- apps/api/src/routes/reviewRoutes.js
+++ apps/api/src/routes/reviewRoutes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/auth.js";
 import { getReviews, postReview } from "../controllers/reviewController.js";
 
 export const reviewRoutes = Router();
+
+reviewRoutes.use(authMiddleware);
 
 reviewRoutes.get("/", getReviews);
 reviewRoutes.post("/", postReview);
